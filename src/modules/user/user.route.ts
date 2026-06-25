b2296aec2ca8d9cd996cea9f9b5d8ce1d5af13dc
Rupok Hossain Siam <rh.siam999@gmail.com>
2026-06-25 13:36:19 +0600

    Cleaning And Organizing The Auth Middleware And Implementing Update User Profile API

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -8,80 +8,11 @@ import { jwtUtils } from "../../utils/jwt";
 import { Role } from "../../../generated/prisma/enums";
 import { catchAsync } from "../../utils/catchAsync";
 import { JwtPayload } from "jsonwebtoken";
+import { auth } from "../../middlewares/auth";
 
 const router = Router();
 
-declare global {
-  namespace Express {
-    interface Request {
-      user?: {
-        email: string;
-        name: string;
-        id: string;
-        role: Role;
-      };
-    }
-  }
-}
-
 router.post("/register", userController.registerUser);
-
-const auth = (...requiredRoles: Role[]) => {
-  return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.cookies.accessToken;
-    //   || req.headers.authorization?.startsWith("Bearer")
-    //     ? req.headers.authorization?.split(" ")[1]
-    //     : req.headers.authorization;
-
-    if (!token) {
-      throw new Error(
-        "You are not logged in. Please log in to access this resource.",
-      );
-    }
-
-    const verifiedToken = jwtUtils.verifiedToken(
-      token,
-      config.jwt_access_secret,
-    );
-
-    if (!verifiedToken.success) {
-      throw new Error(verifiedToken.error);
-    }
-
-    const { email, name, id, role } = verifiedToken.data as JwtPayload;
-
-    if (requiredRoles.length && !requiredRoles.includes(role)) {
-      throw new Error(
-        "Forbidden. You don't have permission to access this resource.",
-      );
-    }
-
-    const user = await prisma.user.findUnique({
-      where: {
-        id,
-        email,
-        name,
-        role
-      },
-    });
-
-    if (!user) {
-      throw new Error("User not found. Please log in again.");
-    }
-
-    if (user.activeStatus === "BLOCKED") {
-      throw new Error("Your account has been blocked. Please contact support.");
-    }
-
-    req.user = {
-        email, 
-        name,
-        id,
-        role
-    }
-    next();
-  });
-};
 
 router.get(
   "/me",
@@ -127,6 +58,12 @@ router.get(
   auth(Role.ADMIN, Role.USER, Role.AUTHOR),
 
   userController.getMyProfile,
+);
+
+router.put(
+  "/my-profile",
+  auth(Role.ADMIN, Role.USER, Role.AUTHOR),
+  userController.updateMyProfile,
 );
 
 export const userRoutes = router;
