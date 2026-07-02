46fd407cf4cd9e33d6371cbf71a68c7a0abba492
Rupok Hossain Siam <rh.siam999@gmail.com>
2026-07-03 01:46:01 +0600

    Organizing Stripe Webhook Endpoint And Exploring Event Types Documentation

--- src/modules/subscription/subscription.service.ts
+++ src/modules/subscription/subscription.service.ts
@@ -1,3 +1,4 @@
+import Stripe from "stripe";
 import config from "../../config";
 import { prisma } from "../../lib/prisma";
 import { stripe } from "../../lib/stripe";
@@ -58,6 +59,55 @@ const createCheckOutSession = async (userId: string) => {
   };
 };
 
+const handleWebhook = async (payload: Buffer, signature: string) => {
+  const endpointSecret = config.stripe_webhook_secret;
+
+  const event = stripe.webhooks.constructEvent(
+    payload,
+    signature,
+    endpointSecret,
+  );
+
+  // Handle the event
+  switch (event.type) {
+    // Occurs when a Checkout Session has been successfully completed.
+    case "checkout.session.completed":
+      console.log("Webhook Hit");
+      console.log(event.data.object);
+      const session: Stripe.Checkout.Session = event.data.object;
+      const userId = session.metadata?.userId;
+      const stripeCustomerId = session.customer;
+      const stripeSubscriptionId = session.subscription as string;
+
+      if (!userId || !stripeSubscriptionId || !stripeCustomerId) {
+        throw new Error("Webhook Failed");
+      }
+
+      const stripeSubscription = await stripe.subscriptions.retrieve(stripeSubscriptionId);
+
+
+      console.log("sub info", stripeSubscription)
+
+      // const paymentIntent = event.data.object;
+
+      break;
+
+    //Occurs whenever a subscription changes (e.g., switching from one plan to another, or changing the status from trial to active).
+    case "customer.subscription.updated":
+      // const paymentMethod = event.data.object;
+
+      break;
+
+    //Occurs whenever a customer’s subscription ends.
+    case "customer.subscription.deleted":
+      break;
+    default:
+      // Unexpected event type
+      console.log(`No Event Matched. Unhandled event type ${event.type}.`);
+  }
+};
+
 export const subscriptionServices = {
   createCheckOutSession,
+  handleWebhook,
 };
